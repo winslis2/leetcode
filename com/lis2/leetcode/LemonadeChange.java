c8e69d20d93a45274f4cdc7237dcfe6a7ac61ac2
kunmingqaq@163.com <kunmingqaq@163.com>
2020-12-10 17:24:46 +0800

    柠檬水找零

---
+++ com/lis2/leetcode/LemonadeChange.java
@@ -0,0 +1,9 @@
+package com.lis2.leetcode;
+//柠檬水找零
+//https://leetcode-cn.com/problems/lemonade-change/
+public class LemonadeChange {
+
+    public boolean lemonadeChange(int[] bills) {
+        if (bills[0] != 5) return false;
+    }
+}
