5e3be736a0a4503f783e5c08705c7266dc978644
kunmingqaq@163.com <kunmingqaq@163.com>
2020-12-29 11:11:29 +0800

    买卖股票的最佳时机 IV[未完成]

---
+++ com/lis2/leetcode/MixProfit.java
@@ -0,0 +1,22 @@
+package com.lis2.leetcode;
+
+import org.junit.Test;
+
+public class MixProfit {
+    @Test
+    public void test() {
+        int k = 2;
+        int[] prices = {3,2,6,5,0,3};
+
+    }
+    // 188.买卖股票的最佳时机 IV https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/
+    //只要右边比左边的大就可以买
+    //2 4 7 怎么卖最多都是5
+    //k 要指定完成k笔交易
+
+
+    //答案都看不懂的那种
+    public int maxProfit(int k, int[] prices) {
+        return 0;
+    }
+}
