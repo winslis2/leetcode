92fc683e5040f4a1883e641df130b0eef964a2f8
winslis2 <kunmingqaq@163.com>
2021-01-27 12:26:51 +0800

    隔天做移动零题目

---
+++ com/lis2/leetcodenextday/MoveZeroes.java
@@ -0,0 +1,32 @@
+package com.lis2.leetcodenextday;
+
+import org.junit.Test;
+
+import javax.management.StandardEmitterMBean;
+import java.util.Arrays;
+
+public class MoveZeroes {
+    @Test
+    public void test() {
+        int[] nums = {0,1,0,3,12};
+        moveZeroes(nums);
+    }
+
+    public void moveZeroes(int[] nums) {
+        int left=0,right=0;
+        for (int num : nums) {
+            if (num != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void swap(int[] nums,int left,int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
